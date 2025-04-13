373f9e31e39ed52ba58fd41b04db607bf3955f18
Shahadat Islam <md6960089r@gmail.com>
2025-04-13 10:34:49 +0600

    Add files via upload

---
+++ Script/events/antiout.js
@@ -0,0 +1,24 @@
+module.exports.config = {
+ name: "antiout",
+ eventType: ["log:unsubscribe"],
+ version: "0.0.1",
+ credits: "Nazrul",
+ description: "Listen events Notify bot or group member with random gif/photo/video"
+};
+
+module.exports.run = async({ event, api, Threads, Users }) => {
+ let data = (await Threads.getData(event.threadID)).data || {};
+ if (data.antiout == false) return;
+ if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
+ const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
+ const type = (event.author == event.logMessageData.leftParticipantFbId) ? "self-separation" : "being kicked by the administrator";
+ if (type == "self-separation") {
+  api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
+   if (error) {
+    api.sendMessage(`সরি বস😞😞😞😞\n${name}\nব্লক করছে অথবা তার আইডিতে মেসেঞ্জার অপশন তাই এড করতে পারলাম না😞😞\n⋆✦⎯⎯⎯⎯⎯⎯⎯⎯✦⋆\n𝐂𝐫𝐞𝐚𝐭𝐨𝐫 ➢ 𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐈𝐬𝐥𝐚𝐦`, event.threadID)
+   } else api.sendMessage(`লিফ্ট নেওয়া  ${name}  কে  পুনরায় এ্যাড করা হইছে\n⋆✦⎯⎯⎯⎯⎯⎯⎯⎯✦⋆\n𝐂𝐫𝐞𝐚𝐭𝐨𝐫 ➢ 𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐈𝐬𝐥𝐚𝐦 `,
+
+ event.threadID);
+  })
+ }
+}
---
+++ Script/events/leave.js
@@ -0,0 +1,34 @@
+module.exports.config = {
+  name: "leave",
+  eventType: ["log:unsubscribe"],
+  version: "1.0.0",
+  credits: "nazrul",
+  description: "Thông báo bot hoặc người rời khỏi nhóm",
+  dependencies: {
+    "fs-extra": "",
+    "path": ""
+  }
+};
+
+module.exports.run = async function({ api, event, Users, Threads }) {
+  if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
+  const { createReadStream, existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+  const { join } =  global.nodemodule["path"];
+  const { threadID } = event;
+  const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
+  const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
+  const type = (event.author == event.logMessageData.leftParticipantFbId) ? "  তোর সাহস কম না এখানে 𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐈𝐬𝐥𝐚𝐦 এর  পারমিশন ছাড়া তুই লিভ  নিস😡😠🤬 \n\n⋆✦⎯⎯⎯⎯⎯⎯⎯⎯✦⋆\n𝐂𝐫𝐞𝐚𝐭𝐨𝐫 ➢ 𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭" : "তোমার এই গ্রুপে থাকার কোনো যোগ্যাতা নেই ছাগল😡।\nতাই তোমার লাথি মেরে গ্রুপ থেকে বের করে দেওয়া হলো🤪। WELLCOME REMOVE🤧\n⋆✦⎯⎯⎯⎯⎯⎯⎯⎯✦⋆\n𝐂𝐫𝐞𝐚𝐭𝐨𝐫 ➢ 𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭";
+  const path = join(__dirname, "Nazrul", "leaveGif");
+  const gifPath = join(path, `leave1.gif`);
+  var msg, formPush
+
+  if (existsSync(path)) mkdirSync(path, { recursive: true });
+
+  (typeof data.customLeave == "undefined") ? msg = "ইস {name} {type} " : msg = data.customLeave;
+  msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
+
+  if (existsSync(gifPath)) formPush = { body: msg, attachment: createReadStream(gifPath) }
+  else formPush = { body: msg }
+
+  return api.sendMessage(formPush, threadID);
+}
